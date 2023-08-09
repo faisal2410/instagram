a345cd216e7ccb0e4fb617317df09cf8ff2d0044
Faisal ahmed <faisal2410@yahoo.com>
2023-08-09 12:40:03 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {onMounted} from "vue"
+import Nav from "./components/Nav.vue"
+import {RouterView} from "vue-router"
+import {useUserStore} from "./stores/users"
+
+const userStore = useUserStore()
+
+onMounted(() => {
+  userStore.getUser()
+})
+</script>
+
+<template>
+  <main>
+    <Nav />
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+</style>
